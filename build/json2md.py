2e16c77347ef5a429058469fd82819386f6cf5b6
赵璟 <zhaojing_cathay@163.com>
2024-01-24 17:31:47 +0800

    Update json2md.py

--- build/json2md.py
+++ build/json2md.py
@@ -11,33 +11,30 @@ with open(json_file_path) as json_file:
     data = json.load(json_file)
 
 # Create a dictionary to hold the titles and their corresponding keys and trademarks
-# and a set to keep track of added sub-titles to avoid duplicates
 catalog_dict = {}
-added_sub_titles = set()
 
 # Traverse the JSON data to fill the dictionary
 for entry in data:
-    key = entry['key']
+    parent_key = entry['key']  # This is the key for the parent item
     trademark = entry['trademark']
     items = entry['catalogCollection']['items']
     for item in items:
-        title = item['title']
+        # Check if the item has a 'catalogCollection' and process accordingly
         if 'catalogCollection' in item:
             subitems = item['catalogCollection']['items']
             for subitem in subitems:
                 sub_title = subitem['title']
-                # Only add the sub_title if it hasn't been added before
-                if sub_title not in added_sub_titles:
-                    added_sub_titles.add(sub_title)
-                    if sub_title not in catalog_dict:
-                        catalog_dict[sub_title] = []
-                    catalog_dict[sub_title].append((trademark, key))
+                # Initialize the set for this sub_title if it hasn't been added before
+                if sub_title not in catalog_dict:
+                    catalog_dict[sub_title] = set()
+                # Add the (trademark, parent_key) tuple to the set for this sub_title
+                catalog_dict[sub_title].add((trademark, parent_key))
 
 # Now, generate the Markdown content
 markdown_content = ""
-for title, pairs in catalog_dict.items():
+for title, pairs in sorted(catalog_dict.items()):
     markdown_content += f"## {title}\n\n"
-    for trademark, key in pairs:
+    for trademark, key in sorted(pairs):
         markdown_content += f"- [{trademark}](/apps/{key})\n\n"
 
 # Write the Markdown content to the specified output file
