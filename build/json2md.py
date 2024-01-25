d42ffbf762eab5a2598fb53ba8927a912573efc3
赵璟 <zhaojing_cathay@163.com>
2024-01-25 10:46:33 +0800

    Update json2md.py

--- build/json2md.py
+++ build/json2md.py
@@ -6,6 +6,25 @@ json_file_path = sys.argv[1]
 # The second command-line argument is the output Markdown file name
 output_md_file_name = sys.argv[2]
 
+# Define the fixed content for both languages
+fixed_content_zh = (
+    "# 应用\n\n"
+    "我们提供了数百个热门开源应用的自动化部署包，请点击下面的类目详细了解。\n\n"
+    "如果您期望的应用不在类目中，欢迎给我们反馈。如果您是优质应用供应商，"
+    "欢迎[加入](http://www.websoft9.com)到我们的目录中来。\n\n"
+)
+
+fixed_content_en = (
+    "# Apps\n\n"
+    "Docs of hundreds of pre-packaged apps below\n\n"
+)
+
+# Check the language from the file name or another indicator
+if 'zh' in output_md_file_name:
+    fixed_content = fixed_content_zh
+else:  # Default to English if 'zh' is not found
+    fixed_content = fixed_content_en
+
 # Load the JSON data
 with open(json_file_path) as json_file:
     data = json.load(json_file)
@@ -41,4 +60,4 @@ for title, info in sorted(catalog_dict.items(), key=lambda x: min(x[1]['position
 
 # Write the Markdown content to the specified output file
 with open(output_md_file_name, 'w') as md_file:
-    md_file.write(markdown_content)
+    md_file.write(fixed_content + markdown_content)
