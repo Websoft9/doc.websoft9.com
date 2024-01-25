8473b3894f0bea231c7a7126559773d0e39cf54e
赵璟 <zhaojing_cathay@163.com>
2024-01-25 09:34:15 +0800

    Update json2md.py

--- build/json2md.py
+++ build/json2md.py
@@ -10,7 +10,7 @@ output_md_file_name = sys.argv[2]
 with open(json_file_path) as json_file:
     data = json.load(json_file)
 
-# Create a dictionary to hold the titles and their corresponding keys and trademarks
+# Create a dictionary to hold the titles and their corresponding keys, trademarks, and positions
 catalog_dict = {}
 
 # Traverse the JSON data to fill the dictionary
@@ -24,17 +24,19 @@ for entry in data:
             subitems = item['catalogCollection']['items']
             for subitem in subitems:
                 sub_title = subitem['title']
-                # Initialize the set for this sub_title if it hasn't been added before
+                position = subitem.get('position', 0)  # Use 0 as a default if position is not specified
+                # Initialize the dictionary for this sub_title if it hasn't been added before
                 if sub_title not in catalog_dict:
-                    catalog_dict[sub_title] = set()
-                # Add the (trademark, parent_key) tuple to the set for this sub_title
-                catalog_dict[sub_title].add((trademark, parent_key))
+                    catalog_dict[sub_title] = {'positions': [], 'entries': set()}
+                # Add position and the (trademark, parent_key) tuple to the dictionary for this sub_title
+                catalog_dict[sub_title]['positions'].append(position)
+                catalog_dict[sub_title]['entries'].add((trademark, parent_key))
 
-# Now, generate the Markdown content
+# Now, generate the Markdown content sorted by position in ascending order
 markdown_content = ""
-for title, pairs in sorted(catalog_dict.items()):
+for title, info in sorted(catalog_dict.items(), key=lambda x: min(x[1]['positions'])):
     markdown_content += f"## {title}\n\n"
-    for trademark, key in sorted(pairs):
+    for trademark, key in sorted(info['entries']):
         markdown_content += f"- [{trademark}](/apps/{key})\n\n"
 
 # Write the Markdown content to the specified output file
