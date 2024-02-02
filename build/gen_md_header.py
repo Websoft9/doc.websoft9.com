6cc3463bc8823ef5cb693ca0e1bd77cedc73492d
赵璟 <zhaojing_cathay@163.com>
2024-02-02 10:35:46 +0800

    Update gen_md_header.py

--- build/gen_md_header.py
+++ build/gen_md_header.py
@@ -12,7 +12,7 @@ client = Client(CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN)
 # 设置查询数据
 content_type_id = 'product'
 # entries = client.entries({'content_type': content_type_id})
-entries = client.entries({'content_type': content_type_id, 'include': 3})
+entries = client.entries({'content_type': content_type_id, 'include': 2})
 
 # 设置 Jinja2 模板环境
 TEMPLATE_DIR = 'docs/apps/_include'
