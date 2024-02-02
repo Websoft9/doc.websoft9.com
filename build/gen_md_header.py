e514c1ca4ad4eda44f2cafa19ae6a256ef41af82
赵璟 <zhaojing_cathay@163.com>
2024-02-02 11:50:11 +0800

    Update gen_md_header.py

--- build/gen_md_header.py
+++ build/gen_md_header.py
@@ -9,10 +9,8 @@ parser.add_argument('template_file', help='The template file to use for generati
 args = parser.parse_args()
 
 # 设置 Jinja2 模板环境
-TEMPLATE_DIR = 'docs/apps/_include'
 TEMPLATE_FILE = args.template_file
-env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
-template = env.get_template(TEMPLATE_FILE)
+template = os.path.basename(TEMPLATE_FILE)
 
 # 根据模板文件名判断使用的语言
 locale = 'zh-CN' if 'zh' in TEMPLATE_FILE else 'en-US'
