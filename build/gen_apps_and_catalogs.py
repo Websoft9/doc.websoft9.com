fe5c1d6219fc78b01fc745d27fef8b911caa03b8
赵璟 <zhaojing_cathay@163.com>
2024-03-25 09:25:06 +0800

    Update gen_apps_and_catalogs.py

--- build/gen_apps_and_catalogs.py
+++ build/gen_apps_and_catalogs.py
@@ -15,7 +15,7 @@ def fetch_catalog_hierarchy(catalog_id, hierarchy=None):
     hierarchy.insert(0, catalog_entry.name)
     
     # 如果存在父目录，递归调用
-    if hasattr(catalog_entry, 'parent_catalog'):
+    if hasattr(catalog_entry, 'catalog') and catalog_entry.parent_catalog:
         fetch_catalog_hierarchy(catalog_entry.parent_catalog.sys.id, hierarchy)
     
     return hierarchy
@@ -23,13 +23,14 @@ def fetch_catalog_hierarchy(catalog_id, hierarchy=None):
 def generate_allcatalogs_md(products):
     lines = []
     for product in products:
-        if hasattr(product, 'catalogs'):
-            for catalog_id in product.catalogs:
+        if hasattr(product, 'catalog'):
+            for catalog_link in product.catalog:
+                catalog_id = catalog_link.sys.id
                 # 获取目录层级
-                hierarchy = fetch_catalog_hierarchy(catalog_id.sys.id)
+                hierarchy = fetch_catalog_hierarchy(catalog_id)
                 if len(hierarchy) > 1:
                     # 构建 Markdown 行
-                    line = f"## {hierarchy[-2]}\n\n- [{hierarchy[-1]}](https://www.websoft9.com/apps/{product.key})"
+                    line = f"## {hierarchy[-2]}\n\n- [{hierarchy[-1]}](https://www.websoft9.com/apps/{product.key.lower()})"
                     lines.append(line)
     return '\n\n'.join(lines)
 
@@ -42,8 +43,8 @@ trademarks = list({entry.trademark for entry in products if hasattr(entry, 'trad
 # 按首字母升序排序
 trademarks.sort(key=lambda x: x.lower())
 
-# 转换为 Markdown 格式
-markdown_content_apps = '\n'.join(f"- {trademark}" for trademark in trademarks)
+# 转换为一行以逗号分隔的 Markdown 格式
+markdown_content_apps = ', '.join(trademarks)
 
 # 输出到 allapps.md 文件
 output_dir = 'i18n/en/docusaurus-plugin-content-docs/current/apps/_include'
