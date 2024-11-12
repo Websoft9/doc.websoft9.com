7e6d784be4962bc575d3be1524b3adc642fff590
赵璟 <zhaojing_cathay@163.com>
2024-11-12 11:06:02 +0800

    Update Layout.js
    Signed-off-by: 赵璟 <zhaojing_cathay@163.com>

--- src/theme/Layout.js
+++ src/theme/Layout.js
@@ -1,29 +1,29 @@
-// src/theme/Layout.js
-import { useLocation } from '@docusaurus/router';
-import OriginalLayout from '@theme-original/Layout';
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+// // src/theme/Layout.js
+// import { useLocation } from '@docusaurus/router';
+// import OriginalLayout from '@theme-original/Layout';
+// import React, { useEffect } from 'react';
+// import { useHistory } from 'react-router-dom';
 
-function useRemoveTrailingSlash() {
-    const location = useLocation();
-    const history = useHistory();
+// function useRemoveTrailingSlash() {
+//     const location = useLocation();
+//     const history = useHistory();
 
-    useEffect(() => {
-        const { pathname, search, hash } = location;
-        if (pathname.length > 1 && pathname.endsWith('/')) {
-            const newPath = pathname.slice(0, -1) + search + hash;
-            console.log(`old pathname: ${pathname}, Redirecting to: ${newPath}`);
-            // 使用 history.replace 以避免页面刷新
-            history.replace(newPath);
-        }
-    }, [location, history]);
-}
+//     useEffect(() => {
+//         const { pathname, search, hash } = location;
+//         if (pathname.length > 1 && pathname.endsWith('/')) {
+//             const newPath = pathname.slice(0, -1) + search + hash;
+//             console.log(`old pathname: ${pathname}, Redirecting to: ${newPath}`);
+//             // 使用 history.replace 以避免页面刷新
+//             history.replace(newPath);
+//         }
+//     }, [location, history]);
+// }
 
-export default function Layout(props) {
-    useRemoveTrailingSlash();
+// export default function Layout(props) {
+//     useRemoveTrailingSlash();
 
-    return <OriginalLayout {...props} />;
-}
+//     return <OriginalLayout {...props} />;
+// }
 
-//window.history.replaceState(null, '', newPath);
-//window.location.replace(newPath);
+// //window.history.replaceState(null, '', newPath);
+// //window.location.replace(newPath);
